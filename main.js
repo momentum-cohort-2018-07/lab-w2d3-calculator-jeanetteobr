bfe3dcfc315c7133e0d2b8b286156ea59e4a1fd3
jeanetteobr <jeanette.obr@gmail.com>
2018-08-08 21:02:55 -0400

    Got JS working for number divs to dispaly in empty div onthe calc.

--- main.js
+++ main.js
@@ -1,2 +1,51 @@
+document.querySelector('.clear').addEventListener('click', function(){
+    document.querySelector('.display').innerHTML=''})
+
+document.querySelector('.num-7').addEventListener('click', function(){
+    document.querySelector('.display').innerHTML+='7'})
+
+document.querySelector('.num-8').addEventListener('click', function(){
+    document.querySelector('.display').innerHTML+='8'})
+
+document.querySelector('.num-9').addEventListener('click', function(){
+    document.querySelector('.display').innerHTML+='9'})
+
+document.querySelector('.num-4').addEventListener('click', function(){
+    document.querySelector('.display').innerHTML+='4'})
+
+document.querySelector('.num-5').addEventListener('click', function(){
+    document.querySelector('.display').innerHTML+='5'})
+
+document.querySelector('.num-6').addEventListener('click', function(){
+    document.querySelector('.display').innerHTML+='6'})
+
 document.querySelector('.num-1').addEventListener('click', function(){
     document.querySelector('.display').innerHTML+='1'})
+ 
+document.querySelector('.num-2').addEventListener('click', function(){
+    document.querySelector('.display').innerHTML+='2'})
+
+document.querySelector('.num-3').addEventListener('click', function(){
+    document.querySelector('.display').innerHTML+='3'})
+
+document.querySelector('.num-0').addEventListener('click', function(){
+    document.querySelector('.display').innerHTML+='0'})
+
+document.querySelector('.dec').addEventListener('click', function(){
+    document.querySelector('.display').innerHTML+='.'})
+
+document.querySelector('.multiply').addEventListener('click', function(){
+    document.querySelector('.display').innerHTML+='*'})
+
+document.querySelector('.divide').addEventListener('click', function(){
+    document.querySelector('.display').innerHTML ='/'})
+
+document.querySelector('.minus').addEventListener('click', function(){
+    document.querySelector('.display').innerHTML+='+'})
+
+document.querySelector('.equals').addEventListener('click', function(){
+    document.querySelector('.display').innerHTML+='0'})
+
+document.querySelector('.plus').addEventListener('click', function(){
+    document.querySelector('.display').innerHTML+='+'})
+
