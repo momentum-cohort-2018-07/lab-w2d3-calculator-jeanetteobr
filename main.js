48afc234981de2c5c9120f2672f129f163b4f106
jeanetteobr <jeanette.obr@gmail.com>
2018-08-08 23:07:40 -0400

    Got multiplication operator button to return 'undefined' in display div using eval()

--- main.js
+++ main.js
@@ -1,51 +1,68 @@
-document.querySelector('.clear').addEventListener('click', function(){
-    document.querySelector('.display').innerHTML=''})
+document.querySelector('.clear').addEventListener('click', function () {
+    document.querySelector('.display').innerHTML = ''
+})
 
-document.querySelector('.num-7').addEventListener('click', function(){
-    document.querySelector('.display').innerHTML+='7'})
+document.querySelector('.num-7').addEventListener('click', function () {
+    document.querySelector('.display').innerHTML += '7'
+})
 
-document.querySelector('.num-8').addEventListener('click', function(){
-    document.querySelector('.display').innerHTML+='8'})
+document.querySelector('.num-8').addEventListener('click', function () {
+    document.querySelector('.display').innerHTML += '8'
+})
 
-document.querySelector('.num-9').addEventListener('click', function(){
-    document.querySelector('.display').innerHTML+='9'})
+document.querySelector('.num-9').addEventListener('click', function () {
+    document.querySelector('.display').innerHTML += '9'
+})
 
-document.querySelector('.num-4').addEventListener('click', function(){
-    document.querySelector('.display').innerHTML+='4'})
+document.querySelector('.num-4').addEventListener('click', function () {
+    document.querySelector('.display').innerHTML += '4'
+})
 
-document.querySelector('.num-5').addEventListener('click', function(){
-    document.querySelector('.display').innerHTML+='5'})
+document.querySelector('.num-5').addEventListener('click', function () {
+    document.querySelector('.display').innerHTML += '5'
+})
 
-document.querySelector('.num-6').addEventListener('click', function(){
-    document.querySelector('.display').innerHTML+='6'})
+document.querySelector('.num-6').addEventListener('click', function () {
+    document.querySelector('.display').innerHTML += '6'
+})
 
-document.querySelector('.num-1').addEventListener('click', function(){
-    document.querySelector('.display').innerHTML+='1'})
- 
-document.querySelector('.num-2').addEventListener('click', function(){
-    document.querySelector('.display').innerHTML+='2'})
+document.querySelector('.num-1').addEventListener('click', function () {
+    document.querySelector('.display').innerHTML += '1'
+})
 
-document.querySelector('.num-3').addEventListener('click', function(){
-    document.querySelector('.display').innerHTML+='3'})
+document.querySelector('.num-2').addEventListener('click', function () {
+    document.querySelector('.display').innerHTML += '2'
+})
 
-document.querySelector('.num-0').addEventListener('click', function(){
-    document.querySelector('.display').innerHTML+='0'})
+document.querySelector('.num-3').addEventListener('click', function () {
+    document.querySelector('.display').innerHTML += '3'
+})
 
-document.querySelector('.dec').addEventListener('click', function(){
-    document.querySelector('.display').innerHTML+='.'})
+document.querySelector('.num-0').addEventListener('click', function () {
+    document.querySelector('.display').innerHTML += '0'
+})
 
-document.querySelector('.multiply').addEventListener('click', function(){
-    document.querySelector('.display').innerHTML+='*'})
+document.querySelector('.decimal').addEventListener('click', function () {
+    document.querySelector('.display').innerHTML += '.'
+})
 
-document.querySelector('.divide').addEventListener('click', function(){
-    document.querySelector('.display').innerHTML ='/'})
+document.getElementById('times').addEventListener('click', function () {
+    document.querySelector(".display").innerHTML += eval();
+})
 
-document.querySelector('.minus').addEventListener('click', function(){
-    document.querySelector('.display').innerHTML+='+'})
+document.querySelector('.divide').addEventListener('click', function () {
+    document.querySelector('.display').innerHTML += '/'
+})
 
-document.querySelector('.equals').addEventListener('click', function(){
-    document.querySelector('.display').innerHTML+='0'})
+document.querySelector('.minus').addEventListener('click', function () {
+    document.querySelector('.display').innerHTML += '+'
+})
 
-document.querySelector('.plus').addEventListener('click', function(){
-    document.querySelector('.display').innerHTML+='+'})
+document.querySelector('.equals').addEventListener('click', function () {
+    document.querySelector('.display').innerHTML += ''
+})
+
+document.querySelector('.plus').addEventListener('click', function () {
+    document.querySelector('.display').innerHTML += '+'
+})
 
